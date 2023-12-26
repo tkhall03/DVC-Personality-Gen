9aee83e048dd1fb193c96d95b51b015660a1aefa
lz433 <lucy.zhang.433@gmail.com>
2023-12-26 05:01:20 -0600

    first display!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@
 import {Common} from './dvcComponents/common.js';
 import { useEffect, useState } from 'react';
 
-
 export default function Home() {
   function onLoad()
   {
@@ -16,28 +15,31 @@ export default function Home() {
     Common.setDragonSpineOrImage("/dianu_01_f_adult_pb");
   }
 
+  function loadAuraDragon() {
+    Common.setAuraFront("/aura_front");
+    Common.setAuraBack("/aura_back");
+  }
+
   useEffect(() => {
     // call api or anything
     onLoad();
     loadSpineDragon();
+    loadAuraDragon();
     console.log("loaded");
   },[]);
   return (
-      <div id= 'root' className='background'>
-        <div className='linkButtonContainer'></div>
+      <div id= 'root' className='background flex-col'>
         <div>
           <img className='alignCenter' src="https://res.dvc.land/dvc-web/res/logo.png" alt="" />
-        </div>
-        <div>
-          <img className='alignCenter' src="https://res.dvc.land/dvc-illust/dianu_02_adult.png" alt="" />
         </div>
         <div className='dragonCenter'>
           <div className='dragonField'>
             <img className='alignCenter' src="https://res.dvc.land/dvc-web/res/table.png"  alt=""></img>
           </div>
-          <div className='drgaonDot'>
-            <img id="dragon-image" className='alignCenter' alt="" />
-            <canvas id="canvas-dragon" className="spineCanvas dragonCanvas" width="479" height="856"></canvas>
+          <div className='drgaonDot alignCenter'>
+            <canvas id="canvas-dragon" className="spineCanvas dragonCanvas h-1/4 w-1/4"></canvas>
+            <canvas id="canvas-aura-back" className="spineCanvas auraBackCanvas"></canvas>
+            <canvas id="canvas-aura-front" className="spineCanvas auraFrontCanvas"></canvas>
           </div>
         </div>
       </div>
